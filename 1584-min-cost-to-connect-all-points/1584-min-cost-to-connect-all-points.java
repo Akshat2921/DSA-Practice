0546e7b33af25b03e54a9482ec613aea5b4e4e33
Akshat2921 <akshatjain292103@gmail.com>
2026-06-23 22:52:06 +0530

    Time: 537 ms (29.49%), Space: 76.6 MB (60.08%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
@@ -1,53 +1,29 @@
 class Solution {
     public int minCostConnectPoints(int[][] points) {
         int n=points.length;
-        List<int[]> edges=new ArrayList<>();
+        boolean[] visited=new boolean[n];
+        PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->a[0]-b[0]);
+        pq.offer(new int[]{0,0});
 
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                int dist=Math.abs(points[i][0]-points[j][0])+
-                Math.abs(points[i][1]-points[j][1]);
-                edges.add(new int[]{i,j,dist});
-            }
-        }
-        Collections.sort(edges,(a,b)-> a[2]-b[2]);
-
-        DSU dsu=new DSU(n);
         int cost=0;
 
-        for(int[] edge:edges){
-            if(dsu.union(edge[0],edge[1])){
-                cost+=edge[2];
+        while(!pq.isEmpty()){
+            int[] curr=pq.poll();
+            int currCost=curr[0];
+            int u=curr[1];
+
+            if(visited[u]) continue;
+
+            visited[u]=true;
+            cost+=currCost;
+            for(int v=0;v<n;v++){
+                if(!visited[v]){
+                    int dist = Math.abs(points[u][0] - points[v][0]) +
+                               Math.abs(points[u][1] - points[v][1]);
+                    pq.offer(new int[]{dist, v});
+                }
             }
         }
         return cost;
-    }
-    class DSU{
-        int[] parent,rank;
-        DSU(int n){
-            parent=new int[n];
-            rank=new int[n];
-            for(int i=0;i<n;i++) parent[i]=i;
-        }
-        boolean union(int x,int y){
-            int px=find(x);
-            int py=find(y);
-
-            if(px==py) return false;
-            if(rank[py]<rank[px]){
-                parent[py]=px;
-            }else if(rank[px]<rank[py]){
-                parent[px]=py;
-            }else{
-                parent[py]=px;
-                rank[px]++;
-            }
-            return true;
-        }
-        int find(int x){
-            if(parent[x] != x)
-                parent[x] = find(parent[x]);
-            return parent[x];
-        }
     }
 }
