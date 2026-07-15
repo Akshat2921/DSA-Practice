ddca8c6b00f4777fc89876a4c9034c26b300015c
Akshat2921 <akshatjain292103@gmail.com>
2026-07-15 14:37:11 +0530

    Time: 0 ms (100%), Space: 42 MB (75.98%) - LeetHub

--- 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.java
+++ 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.java
@@ -1,5 +1,6 @@
 class Solution {
     public int minFlips(int a, int b, int c) {
+        /*
         int flips_required=0;
 
         while(a>0 || b>0 || c>0){
@@ -17,5 +18,11 @@ class Solution {
             c>>=1;
         }
         return flips_required;
+        */
+
+        int a_OR_b=a|b;
+        int now_XOR_with_c= a_OR_b ^ c;
+
+        return Integer.bitCount(now_XOR_with_c)+Integer.bitCount(now_XOR_with_c & (a & b));
     }
 }
